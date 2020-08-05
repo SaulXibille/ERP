e9509e2bae9a584d91d766e312214ee4b9adebb5
SaulXibille <49504706+SaulXibille@users.noreply.github.com>
2020-08-05 16:30:08 -0600

    logo

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php echo $titulo;?></title>
+  <link rel="icon"  type="image/png" href="<?php echo base_url('assets/imagenes/Logo.png'); ?>">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
