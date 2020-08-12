d11b27b710da24895d034d99d6101d5192b118cd
SaulXibille <49504706+SaulXibille@users.noreply.github.com>
2020-08-11 19:40:59 -0600

    sada

--- application/views/Productos/productos.php
+++ application/views/Productos/productos.php
@@ -119,6 +119,9 @@
             },
             sProcessing: "Procesando...",
           },
+          lengthMenu: [10, 20, 50, 100],
+          scrollY: 400,
+          scroller: true,
           dom: "<'row' <'col-sm-12 col-md-4'l> <'col-sm-12 col-md-4 excel'B> <'col-sm-12 col-md-4'f> >" +
               "<'row'<'col-sm-12'tr>>" +
               "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
